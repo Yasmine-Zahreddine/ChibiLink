1646bee176f76447cf2bcf21f45f386f96fc7a57
Yasmine-Zahreddine <zahreddineyasmine@gmail.com>
2025-04-07 12:36:26 +0300

    edited imports

--- backend/database.py
+++ backend/database.py
@@ -4,8 +4,8 @@ import os
 from dotenv import load_dotenv
 from logger import logger
 import contextlib
-from utils import generate_short_code, validate_url
-from models import ChibiLinkURLS
+import utils
+import models 
 from datetime import datetime
 load_dotenv()
 
@@ -40,13 +40,13 @@ def get_shortened_url(url:str, base_url: str, expiration_date , one_time_click: 
     try: 
         with session_manager(mainCall) as session:
             print("entering get_shortened_url")
-            if validate_url(url): 
+            if utils.validate_url(url): 
                 logger.info(f"Valid URL: {url}")
-                existing_url = session.query(ChibiLinkURLS).filter(ChibiLinkURLS.original_url == url , ChibiLinkURLS.expiration_date > datetime.now()).first()
+                existing_url = session.query(models.ChibiLinkURLS).filter(models.ChibiLinkURLS.original_url == url , models.ChibiLinkURLS.expiration_date > datetime.now()).first()
                 if existing_url:
                     logger.info(f"URL already exists: {url}")
                     return existing_url.new_url
-                short_code = generate_short_code(url, length)
+                short_code = utils.generate_short_code(url, length)
                 new_url = f"{base_url}{short_code}"
                 data = {
                     "original_url": url,
@@ -56,7 +56,7 @@ def get_shortened_url(url:str, base_url: str, expiration_date , one_time_click: 
                     "one_time_click": one_time_click,
                     "expiration_date": expiration_date, 
                 }
-                new_url_entry = ChibiLinkURLS.from_dict(data)
+                new_url_entry = models.ChibiLinkURLS.from_dict(data)
                 logger.info(f"Creating new URL entry: {new_url}")
                 try:
                     session.add(new_url_entry)
@@ -74,7 +74,7 @@ def get_shortened_url(url:str, base_url: str, expiration_date , one_time_click: 
 
 def get_original_url(short_code: str, mainCall: bool = True):
     with session_manager(mainCall) as session:
-        url_entry = session.query(ChibiLinkURLS).filter(ChibiLinkURLS.short_code == short_code).first()
+        url_entry = session.query(models.ChibiLinkURLS).filter(models.ChibiLinkURLS.short_code == short_code).first()
         if url_entry:
             if url_entry.one_time_click:
                 session.delete(url_entry)
@@ -86,7 +86,7 @@ def get_original_url(short_code: str, mainCall: bool = True):
 
 def delete_url_entry(url_pk:str):
     with session_manager() as session: 
-        url_entry = session.query(ChibiLinkURLS).filter(ChibiLinkURLS.url_pk == url_pk).first()
+        url_entry = session.query(models.ChibiLinkURLS).filter(models.ChibiLinkURLS.url_pk == url_pk).first()
         if url_entry: 
             session.delete(url_entry)
             session.commit()
@@ -94,7 +94,7 @@ def delete_url_entry(url_pk:str):
 
 def add_url_click(url_pk:str):
     with session_manager() as session: 
-        url_entry = session.query(ChibiLinkURLS).filter(ChibiLinkURLS.url_pk == url_pk).first()
+        url_entry = session.query(models.ChibiLinkURLS).filter(models.ChibiLinkURLS.url_pk == url_pk).first()
         if url_entry: 
             url_entry.clicks += 1
             session.commit()
